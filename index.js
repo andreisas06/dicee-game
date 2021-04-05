d83412d2d47e17f6c9f08f6018d8144d766384e5
Andrei Sas <andreisas007@gmail.com>
2021-04-05 19:46:31 +0300

    pretty code

--- index.js
+++ index.js
@@ -14,17 +14,3 @@ if (randomNumber2 === randomNumber1) {
 } else {
 	document.querySelector("h1").textContent = "Player 2 wins! 🏆";
 }
-
-// if(randomNumber1 > randomNumber2){
-//     document.querySelector("h1").textContent
-// }
-
-// if (randomNumber1 > randomNumber2) {
-//     document.querySelector("h1").textContent = "Player 1 Wins!";
-
-//    } else if (randomNumber2 > randomNumber1) {
-//     document.querySelector("h1").textContent = "Player 2 Wins!";
-
-//    } else {
-//    document.querySelector("h1").textContent = "Draw!";
-//    }
